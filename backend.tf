dc59875e29b3316aeb342e33b6bd84fe57ac586c
Sotu <Sotu>
2025-02-22 09:06:55 -0600

    using backend-small-letters

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform{
     backend "s3"{
         # bucket = "github-action-bucket-3"
-        bucket = "GH-actions-bucket-54321"
+        bucket = "gh-actions-bucket-54321"
         key = "file"
         region = "us-east-1"
     }
